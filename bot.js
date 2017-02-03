ca294885e09fd48413865b8702dc76fa52b73845
Haven Barnes <haven38@gmail.com>
2017-02-02 22:48:46 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,13 +9,13 @@ var redditConfig = require('./reddit_config.js');
 var Twitter = new twit(twitConfig);
 var Reddit = new snoowrap(redditConfig);
 
+// Set up Bot to attempt ~ 1 tweet/hr
 setInterval(postWallpaper, 8600000);
 
 function postWallpaper() {
-
-	// Get wallpaper
 	var randomPost = random(0, 20);
 	
+	// Fetch wallpaper
 	Reddit.getSubreddit('wallpapers').getHot()[randomPost].url.then(url => {
 		
 		var imageURL = './wallpapers/' + url.substring(url.length - 9, url.length - 1)
@@ -25,7 +25,10 @@ function postWallpaper() {
 			return;
 		}
 
+		// Begin Downloading Image
 		var imageFile = fs.createWriteStream(imageURL);
+		
+		// Handle http/https
 		if (url.indexOf('https') > -1) {
 			var request = https.get(url, function(response) {
 				response.pipe(imageFile);
@@ -35,27 +38,34 @@ function postWallpaper() {
 			});
 		} else {
 			var request = http.get(url, function(response) {
-			response.pipe(imageFile);
-			imageFile.on('finish', function() {
-				tweet(imageURL);
+				response.pipe(imageFile);
+				imageFile.on('finish', function() {
+					tweet(imageURL);
+				});
 			});
-		});
-	}
+		}
 	});
-	
 }
 
+/**
+ * Composes and sends tweet consisting soley of a wallpaper downloaded previously
+ *
+ * imageURL: Local Image Path to be tweeted out
+ */
 function tweet(imageURL) {
+	// Upload Image File to get media_id
 	Twitter.postMediaChunked({ file_path: imageURL }, function (err, data, response) {
 		if (data != null) {
 			console.log(data.media_id_string)
 			var mediaIDString = data.media_id_string
 			var meta_params = { media_id: mediaIDString, alt_text: { text: 'wallpaper'}}
 			
+			// Create Media type to be attached to Tweet
 			Twitter.post('media/metadata/create', meta_params, function (err, data, response) {
 
 				var params = { status: '', media_ids: [mediaIDString]}
 					
+				// Create Tweet, attach Media via params
 				Twitter.post('statuses/update', params, function(err, data, response) {
 					console.log(data)
 				});
@@ -63,6 +73,7 @@ function tweet(imageURL) {
 		}
 	});		
 }
+
 
 function random(low, high) {
     return Math.floor(Math.random() * (high - low) + low);
